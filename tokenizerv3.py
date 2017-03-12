02d771de6a5fabfe5516b89e8ebc8fd67cbd6989
codelingoblog <codelingoblog@gmail.com>
2017-03-13 01:33:01 +0530

    Update tokenizerv3.py

--- tokenizerv3.py
+++ tokenizerv3.py
@@ -1,8 +1,8 @@
-#Top 10 most commonly occuring words in Harry Potter and the Sorcerer's Stone.
+#Top 10 most commonly occuring words in Alice's Adventures in Wonderland, Lewis Carrol.
 import re
 from collections import Counter					#Import Counter into our program.
 regex = "([a-zA-Z]+)"
-file = open("HarryPotter1.txt","r")				#Open the book, stored as a text file.	
+file = open("alice.txt","r")				#Open the book, stored as a text file.	
 sentence = file.read().lower()					#Read the contents of book, convert all letters to their lower case and store them in a string.				
 tokenized_words = re.findall(regex,sentence)	
 wordcount = Counter(tokenized_words)			#Store words and their word counts in a Counter.
